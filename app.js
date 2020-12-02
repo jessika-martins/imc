62eb96739a31ffd578b00aa39dacdccacbe716c2
Flávio Micheletti <flavio.micheletti@midway.com.br>
2020-12-01 23:34:25 -0300

    app.js

--- app.js
+++ app.js
@@ -1,5 +1,22 @@
+//
+// Calcular o IMC
+//
+function imc(peso, altura) {
 
-function pesoimc(peso,altura){
-imc = peso /Math.pow(altura,2);
+    peso   = parseFloat(peso);
+    altura = parseFloat(altura);
+
+    let imc = peso / Math.pow(altura, 2);
+
+    if (imc < 18.5) {
+        return "Abaixo do peso";
+    } else if ((imc > 18.5) && (imc < 24.9)) {
+        return "Normal";
+    } else if ((imc > 24.9) && (imc < 30)) {
+        return "Sobrepeso";
+    } else if (imc > 30) {
+        return "Obesidade";
+    }
 }
-module.exports.pesoimc = pesoimc;
+
+// module.exports = imc;
